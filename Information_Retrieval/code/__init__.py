41f40e814fe38459387800ae2d3c711ab3eb52f0
KaiBaeuerle <kaibauerle@gmail.com>
2021-04-13 13:17:59 +0200

    changed it into methods and added personal data-path

--- Information_Retrieval/code/__init__.py
+++ Information_Retrieval/code/__init__.py
@@ -1,5 +1,8 @@
+import example_pointwise
 def main():
     print("start here!")
+    example_pointwise.data_import()
+
 
 if __name__ == "__main__":
     main()
--- Information_Retrieval/code/example_pointwise.py
+++ Information_Retrieval/code/example_pointwise.py
@@ -4,39 +4,40 @@ Created on Wed Apr  7 12:07:01 2021
 
 @author: hp
 """
+import os
 
 import pandas as pd
 
-#%% Read data
 
-path = "F:/University/Information_Retrieval_Project/data/"
+def data_import():
+    # %% Read data
+    path = "/Users/kaibauerle/Desktop/Uni Mannheim/Module/Information Retrieval and Web Search/Project/"
+    # path = "F:/University/Information_Retrieval_Project/data/"
 
-# to concatenate positive and negative examples ("split up" the data set)
-# read positive and then negative entries
-train_pos = pd.read_csv(path + "triples.train.small.tsv",
-                    sep = "\t", nrows = 100, header = None, skiprows = 1, usecols = [0,1])
-train_neg = pd.read_csv(path + "triples.train.small.tsv",
-                    sep = "\t", nrows = 100, header = None, skiprows = 1, usecols = [0,2])  
+    # to concatenate positive and negative examples ("split up" the data set)
+    # read positive and then negative entries
+    train_pos = pd.read_csv(path + "triples.train.small.tsv",
+                            sep="\t", nrows=100, header=None, skiprows=1, usecols=[0, 1])
+    train_neg = pd.read_csv(path + "triples.train.small.tsv",
+                            sep="\t", nrows=100, header=None, skiprows=1, usecols=[0, 2])
 
-#%% Data preprocessing
+    # %% Data pre-processing
 
-train_pos.columns = ["query", "passage"]
-train_neg.columns = ["query", "passage"]
-train_pos["relevant"] = 1 # target label
-train_neg["relevant"] = 0
+    train_pos.columns = ["query", "passage"]
+    train_neg.columns = ["query", "passage"]
+    train_pos["relevant"] = 1  # target label
+    train_neg["relevant"] = 0
 
-train = train_pos.append(train_neg)
+    train = train_pos.append(train_neg)
 
+    # %% Feature computation
 
-#%% Feature computation
+    X = train.copy()
 
-X = train.copy()
+    X["number_chars_q"] = X["query"].apply(count_chars)
+    X["number_chars_p"] = X["passage"].apply(count_chars)
+    print(X)
+
 
 def count_chars(x):
     return len(x)
-
-X["number_chars_q"] = X["query"].apply(count_chars)
-X["number_chars_p"] = X["passage"].apply(count_chars)
-
-
-
