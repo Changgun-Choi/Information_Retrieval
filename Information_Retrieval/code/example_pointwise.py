a59168ef739be51174dd56225268926fc90b2b33
KaiBaeuerle <kaibauerle@gmail.com>
2021-04-15 20:39:34 +0200

    implemented some preprocessing

--- Information_Retrieval/code/example_pointwise.py
+++ Information_Retrieval/code/example_pointwise.py
@@ -7,6 +7,12 @@ Created on Wed Apr  7 12:07:01 2021
 import os
 
 import pandas as pd
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import numpy as np
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+nltk.download()
 
 
 def data_import():
@@ -36,8 +42,59 @@ def data_import():
 
     X["number_chars_q"] = X["query"].apply(count_chars)
     X["number_chars_p"] = X["passage"].apply(count_chars)
-    print(X)
 
+    # preprocseeing
+    X_queries = X.to_numpy()[:,0]
+    X_passages = X.to_numpy()[:,1]
+
+    # lower casing
+
+    # stemming
+    for query in range(len(X_queries)):
+        X_queries[query] = stemSentence(X_queries[query])
+        X_queries[query] = remove_punctoation(X_queries[query])
+    for passage in range(len(X_passages)):
+        X_passages[passage] = stemSentence(X_passages[passage])
+        X_passages[passage] = remove_punctoation(X_passages[passage])
+    # TFIDF & stopwords
+    vectorizer = TfidfVectorizer(stop_words='english')
+    X_queries_preprocessed = vectorizer.fit_transform(X_queries)
+    X_passages_preprocessed = vectorizer.fit_transform(X_passages)
+    X_all_pre = vectorizer.fit_transform(X_queries+X_passages)
+
+    print(X_all_pre.shape)
+    print(X)
+    print(X_queries[0])
+    print(X_queries_preprocessed[0])
+    print(X_passages[0])
+    print(X_passages_preprocessed[0])
+
+
+def stemSentence(sentence):
+    """
+    this method stems the sentences
+    :param sentence: sentences to be stemmed
+    :return: the stemmed sentences
+    """
+    # create an object of PorterStemmer
+    porter = PorterStemmer()
+    token_words = word_tokenize(sentence)
+    stem_sentence = []
+    for word in token_words:
+        stem_sentence.append(porter.stem(word))
+        stem_sentence.append(" ")
+    return "".join(stem_sentence)
+
+def remove_punctoation(string):
+    # initializing punctuations string
+    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    # Removing punctuations in string
+    # Using loop + punctuation string
+    for ele in string:
+        if ele in punc:
+            string = string.replace(ele, "")
+    return string
 
 def count_chars(x):
     return len(x)
