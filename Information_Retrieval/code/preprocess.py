a6193678210aff48b92117e0b378da05ebfbc0e9
MoritzHuu <81373946+MoritzHuu@users.noreply.github.com>
2021-04-16 13:43:05 +0200

    Add files via upload

---
+++ Information_Retrieval/code/preprocess.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 17:44:12 2021
+
+@author: Moritz
+"""
+
+import numpy as np
+#import nltk 
+#nltk.download('stopwords')
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+### Idea: Wrap all the preprocessing steps in one function.
+### Tokenization, PorterStemmer, Punctuation removal and stopword removal are implemented.
+
+def remove_punctuation(string):
+    # initializing punctuations string
+    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    # Removing punctuations in string
+    # Using loop + punctuation string
+    for ele in string:
+        if ele in punc:
+            string = string.replace(ele, "")
+    return string
+
+def preprocess(sentence):
+    """
+
+    Parameters
+    ----------
+    sentence : String.
+
+    Returns
+    -------
+    numpy array. Elements are tokens (Strings).
+
+    """
+    output = []
+    ps = PorterStemmer()
+    stop_words = set(stopwords.words('english')).union(set([""])) 
+             
+    for word in word_tokenize(sentence):
+        w = remove_punctuation(ps.stem(word))
+        if w not in stop_words:
+            output.append(w)
+    
+    return np.array(output)
+
+
+    
