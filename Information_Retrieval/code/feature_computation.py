04ae0de0bb40468533ad25dcf8a9633b74fa4f47
MoritzHuu <81373946+MoritzHuu@users.noreply.github.com>
2021-04-29 16:09:31 +0200

    Add files via upload

--- Information_Retrieval/code/feature_computation.py
+++ Information_Retrieval/code/feature_computation.py
@@ -12,7 +12,7 @@ sys.path.append('/media/Moritz/080FFDFF509A959E/BWsync_share/Master_BW/Informati
 from preprocess import preprocess # own preprocessing function
 
 
-### This file reads the training data, transforms it to a suitable format and calculates bm25 as a feature.
+### This file creates the training data, especially the features based on the query - passage pairs.
 
 
 #%% Read data
@@ -20,28 +20,20 @@ from preprocess import preprocess # own preprocessing function
 path = "/media/Moritz/Seagate Backup Plus Drive/University/Information_Retrieval_Project/data/"
 
 
-# passage data, only used to approximate the average length of a passage
-passages = pd.read_csv(path + "collection.tsv",
-                    sep = "\t", nrows = 1000, header = None)
-number_passages = len(passages)
-
-
-
 # to concatenate positive and negative examples ("split up" the data set)
 # read positive and then negative entries
 train_pos = pd.read_csv(path + "triples.train.small.tsv",
-                    sep = "\t", nrows = 100, header = None, skiprows = 1, usecols = [0,1])
+                    sep = "\t", nrows = 200000, header = None, skiprows = 1, usecols = [0,1])
 train_neg = pd.read_csv(path + "triples.train.small.tsv",
-                    sep = "\t", nrows = 100, header = None, skiprows = 1, usecols = [0,2])  
+                    sep = "\t", nrows = 200000, header = None, skiprows = 1, usecols = [0,2])  
 
 
-# tf_idf for bm25: The file is not final yet! Tf, idf only based on 2 000 000 passages, not the entire corpus.
 def load_obj(name):
     with open(path + name + '.pkl', 'rb') as f:
         return pickle.load(f)
-tf_idf = load_obj("inverted_index/tf_idf_approximated_2000000")
+idf = load_obj("idf")
 
-#%% Data preprocessing
+#%% Data manipulation
 
 
 train_pos.columns = ["query", "passage"]
@@ -53,34 +45,40 @@ train = train_pos.append(train_neg)
 
 
 
-
-#%%
-# passages: estimate average length of passages
-
-passages_bow = np.array([passages[0], passages[1].apply(preprocess)]).transpose()
-total_number_words_in_passages = 0
-for passage in passages_bow.transpose()[1]:
-    total_number_words_in_passages += len(passage)
-avg_len_passages = total_number_words_in_passages / number_passages
-
- 
-
     
 #%% Feature computation
 
 ### define functions to extract features given a query and passage
 
-def bm25(tf_idf, query, passage, avg_len_passages, k = 1.5, b = 0.75): # what is a common value for k?
+def bm25(idf, query, passage, avg_len_passages, k = 1, b = 0.8): 
+    # k, b determined via sequential grid search, see file "bm25_parameter_tuning.py"
     
     query_bow = preprocess(query)
     passage_bow = preprocess(passage)
     common_words = list(set(query_bow) & set(passage_bow))
     bm25 = 0
     for word in common_words: 
-        bm25 += (tf_idf[word]["idf"] * (k + 1) * np.count_nonzero(passage_bow == word) 
+        bm25 += (idf[word] * (k + 1) * np.count_nonzero(passage_bow == word) 
                  / (np.count_nonzero(passage_bow == word) + k * ((1 - b) + b * passage_bow.size/avg_len_passages)))
     return bm25
 
 ### add bm25 of query, passage pair as a feature to the dataset:
 X = train.copy()
-X["bm25"] = X[["query", "passage"]].apply(lambda x: bm25(tf_idf, x[0], x[1], avg_len_passages),axis=1)
+avg_len_passages = 35.4 # See file "estimate_avg_passage_len.py"
+X["bm25"] = X[["query", "passage"]].apply(lambda x: bm25(idf, x[0], x[1], avg_len_passages),axis=1)
+
+X.to_csv(path + "training_pointwise_200000.csv")
+
+#%%
+
+#X.groupby("relevant")["bm25"].describe()
+
+# some bad estimates:
+#a = X.loc[(X["bm25"]>25) & (X["relevant"] ==0)]
+
+#a["query"].values[2]
+#a["passage"].values[2]
+
+
+
+
